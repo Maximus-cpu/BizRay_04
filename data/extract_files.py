0fcf06ef44bfbcbdedd4c25338432d63539e5d3a
Aidan Ryan <aidanryandunn@gmail.com>
2025-10-27 22:16:24 +0100

    streamlined sftp extraction and implemented 64-bit decoding for transformation step

--- data/extract_files.py
+++ data/extract_files.py
@@ -8,8 +8,8 @@ load_dotenv()
 key_path = os.getenv("FTP_KEY_PATH")
 host = os.getenv("FTP_HOST")
 user = os.getenv("FTP_USER")
-remote_dir = "/uploads"
-local_dir = "./data"
+remote_dir = "/uploads/"
+local_dir = "./data/xml_files/"
 
 key = Ed25519Key.from_private_key_file(key_path)
 
@@ -21,6 +21,14 @@ sftp.chdir("/")
 print("Connected. Current dir:", sftp.getcwd())
 
 sftp.chdir("/uploads/000")
+remote_dir += "000/"
+
+head_files_000 = sftp.listdir()[0:20]
+
+for file in head_files_000:
+    if file[-7:] == "XML.xml":
+        sftp.get(remote_dir + file, local_dir + file)
+        print(f"Extracted {file} to {local_dir + file}")
 
 # sftp.get(remote_dir + "/000" + "/000002_6380471306070_000___000_30_7686006_XML.xml", local_dir + "/000002_6380471306070_000___000_30_7686006_XML.xml")
 
