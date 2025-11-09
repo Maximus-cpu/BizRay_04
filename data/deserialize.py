fff73344159ccf6a1c284e52bd93bb0de602259a
Aidan Ryan <aidanryandunn@gmail.com>
2025-11-09 02:23:37 +0100

    Fixed dictionary bugs with deserialization script

--- data/deserialize.py
+++ data/deserialize.py
@@ -4,31 +4,32 @@ import dotenv
 from pprint import pprint
 
 dec_xml_dir = "./data/decoded_xml_files"
-testing_dec_xml = os.path.join(dec_xml_dir, os.listdir(dec_xml_dir)[1])
+testing_dec_xml = os.path.join(dec_xml_dir, os.listdir(dec_xml_dir)[0])
 
-def print_statement(
-    company_name, FNR, legal_structure, total_balance_sheet, fixed_assets, current_assets,
-    prepaid_expenses, equity_capital, provisions, liabilities) -> None:
+# def print_statement(
+#     company_name, FNR, legal_structure, total_balance_sheet, fixed_assets, current_assets,
+#     prepaid_expenses, equity_capital, provisions, liabilities) -> None:
 
-    print(f"Company {company_name} has the following data:")
-    print(f"FNR: {FNR} | legal structure: {legal_structure}")
-    print(f"Their total balance sheet is {total_balance_sheet}")
-    print(f"fixed assets: {fixed_assets}")
-    print(f"current assets: {current_assets}")
-    print(f"prepaid expenses: {prepaid_expenses}\n")
-    print(f"equity capital: {equity_capital}")
-    print(f"provisions: {provisions}")
-    print(f"liabilities: {liabilities}")
+#     print(f"Company {company_name} has the following data:")
+#     print(f"FNR: {FNR} | legal structure: {legal_structure}")
+#     print(f"Their total balance sheet is {total_balance_sheet}")
+#     print(f"fixed assets: {fixed_assets}")
+#     print(f"current assets: {current_assets}")
+#     print(f"prepaid expenses: {prepaid_expenses}\n")
+#     print(f"equity capital: {equity_capital}")
+#     print(f"provisions: {provisions}")
+#     print(f"liabilities: {liabilities}")
 
-with open(testing_dec_xml, "r", encoding="utf-8") as f:
+with open(testing_dec_xml, "rb") as f:
     data = xmltodict.parse(f.read())
     # pprint(data)
     file_info = data.get("UEBERMITTLUNG", {}).get("INFO_DATEN", {})
-    file_info_version = file_info["VERS"] if file_info is not None else None
-    document = data["UEBERMITTLUNG"].get(["BILANZ_GLIEDERUNG"]) 
+    file_info_version = file_info.get("VERS") 
+    document = data["UEBERMITTLUNG"].get("BILANZ_GLIEDERUNG")
+    first_key = list(document.keys())[1]
 
-    if document == "ALLG_JUSTIZ":
-        justice_doc = document
+    if first_key == "ALLG_JUSTIZ":
+        justice_doc = document[first_key]
         doc_version = justice_doc["VERS"]
 
         company = justice_doc["FIRMA"]
@@ -40,7 +41,7 @@ with open(testing_dec_xml, "r", encoding="utf-8") as f:
         filing_year_end = filing_year["ENDE"]
         legal_structure = filing_year["FORM"]
 
-        financial_statement = justice_doc["HGB_Form_2"]
+        financial_statement = document["HGB_Form_2"]
         assets = financial_statement["HGB_224_2"]
         total_balance_sheet = assets["POSTENZEILE"]["BETRAG"]
         fixed_assets = assets["HGB_224_2_A"]["POSTENZEILE"]["BETRAG"]
@@ -52,8 +53,18 @@ with open(testing_dec_xml, "r", encoding="utf-8") as f:
         provisions = equity_items["HGB_224_3_C"]["POSTENZEILE"]["BETRAG"]
         liabilities = equity_items["HGB_224_3_D"]["POSTENZEILE"]["BETRAG"]
 
-        print_statement(company_name, FNR, legal_structure, total_balance_sheet, fixed_assets, current_assets,
-        prepaid_expenses, equity_capital, provisions, liabilities)
+        print(f"Company {company_name} has the following data:")
+        print(f"FNR: {FNR} | legal structure: {legal_structure}")
+        print(f"Their total balance sheet is {total_balance_sheet}")
+        print(f"fixed assets: {fixed_assets}")
+        print(f"current assets: {current_assets}")
+        print(f"prepaid expenses: {prepaid_expenses}\n")
+        print(f"equity capital: {equity_capital}")
+        print(f"provisions: {provisions}")
+        print(f"liabilities: {liabilities}")
+
+        # print_statement(company_name, FNR, legal_structure, total_balance_sheet, fixed_assets, current_assets,
+        # prepaid_expenses, equity_capital, provisions, liabilities)
     
     else:
-        Exception("Finanz Ministry document was found instead of Justice Ministry document!")
+        raise Exception("Finanz Ministry document was found instead of Justice Ministry document!")
