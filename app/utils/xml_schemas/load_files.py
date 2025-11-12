04f021ab6e5f5e5d706da5ebde0df03ae749c9fa
adam-1818-ryan <24IMC11344@fh-krems.ac.at>
2025-11-12 21:56:24 +0100

    Added load_files.py and commented out company seeder

---
+++ app/utils/xml_schemas/load_files.py
@@ -0,0 +1,7 @@
+def load_companies(data):
+    """Creates and commits a Company object from a dictionary. May not function with edge-cases and the dictionary needs to be structured like the Company Model"""
+
+    company = Company(**data)  # uses python's built in unpacking operator
+    db.session.add(company)  # adds the company to the flask database session
+    db.session.commit()
+    return f"company {company.name} has been commited to the database."
